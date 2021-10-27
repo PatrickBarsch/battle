766543a867c222fb246f4bce6796cb5b0e182146
PatrickBarsch <PatrickGB@gmx.de>
2021-10-27 20:55:38 +0200

    Refactored using the PRG pattern and sessions

--- app.rb
+++ app.rb
@@ -5,14 +5,21 @@ class Battle < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_name_1 = params[:player_1]
-    @player_name_2 = params[:player_2]
+    session[:player_name_1] = params[:player_1]
+    session[:player_name_2] = params[:player_2]
+    redirect '/play'
+  end
+
+  get '/play' do 
+    @player_name_1 = session[:player_name_1]
+    @player_name_2 = session[:player_name_2]
     erb :play
   end
 
