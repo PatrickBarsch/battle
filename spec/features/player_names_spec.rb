682e0b43a695ec30fa4b07cfbaf60b7fc139cb53
PatrickBarsch <PatrickGB@gmx.de>
2021-10-28 15:16:41 +0200

    We can see Player 2s Hitpoints

--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,11 +1,18 @@
 feature 'Player name submission' do
-  scenario '' do
-    'Two players can enter their name and it is shown on screen'
+  scenario 'Two players can enter their name and it is shown on screen' do
     visit '/'
     fill_in 'player_1', with: 'Robert'
     fill_in 'player_2', with: 'Andy'
     click_button 'Submit Player Names'
     save_and_open_page
     expect(page).to have_content 'Robert vs Andy'
+  end
+  scenario 'Two players can enter their name and it is shown on screen' do
+    visit '/'
+    fill_in 'player_1', with: 'Robert'
+    fill_in 'player_2', with: 'Andy'
+    click_button 'Submit Player Names'
+    save_and_open_page
+    expect(page).to have_content 'Player 2 Hitpoints: 70'
   end
 end
