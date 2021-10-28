059616116a8a5dc861e5e60bc5787206dd4662ad
PatrickBarsch <PatrickGB@gmx.de>
2021-10-28 15:19:40 +0200

    Refactored to show player 2s actual name with Hitpoints

--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -7,12 +7,12 @@ feature 'Player name submission' do
     save_and_open_page
     expect(page).to have_content 'Robert vs Andy'
   end
-  scenario 'Two players can enter their name and it is shown on screen' do
+  scenario 'We can see Player 2s Hitpoints' do
     visit '/'
     fill_in 'player_1', with: 'Robert'
     fill_in 'player_2', with: 'Andy'
     click_button 'Submit Player Names'
     save_and_open_page
-    expect(page).to have_content 'Player 2 Hitpoints: 70'
+    expect(page).to have_content 'Andy Hitpoints: 70'
   end
 end
